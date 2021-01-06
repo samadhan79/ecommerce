eebf1958c697bcbf0f993d563373ef80ba204dd7
sam <samalexe79@gmail.com>
2021-01-06 21:04:11 +0530

    login

---
+++ app/Http/Controllers/ProductController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class ProductController extends Controller
+{
+    function index(Request $req){
+    	return 'ff';
+    }
+}
--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -3,14 +3,15 @@
 <div class="container custome-login">
 	<div class="row">
 		<div class="col-md-4 col-md-offset-4">
-			<form method="POST">
+			<form method="POST" action="/login">
+				@csrf
 				<div class="form-group">
 					<label for="exampleFormControlInput1" class="form-label">Email address</label>
-					<input type="email" class="form-control" name="email" id="exampleFormControlInput1" placeholder="name@example.com">
+					<input type="email" name="email" class="form-control" name="email" id="exampleFormControlInput1" placeholder="name@example.com">
 				</div>
 				<div class="form-group">
 					<label for="exampleFormControlTextarea1" class="form-label">Password</label>
-					<input type="password" class="form-control" name="password" id="exampleFormControlInput1" placeholder="">
+					<input type="password" name="password" class="form-control" name="password" id="exampleFormControlInput1" placeholder="123">
 				</div>	
 				<div class="mt-5">
 					<input type="submit" class="btn btn-primary" name="">
